9e95ee82c47bb06405c0f1f4ccc97845d5cca9de
Divik <diviktwr4@gmail.com>
2024-10-26 09:33:13 +0530

    outlook addin basic

---
+++ src/commands/commands.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Outlook) {
+    // Assign event handler to add-in command button.
+    console.log("Add-in command is ready.", info);
+  }
+});
+
+/**
+ * Shows a notification when the add-in command is executed.
+ * @param event {Office.AddinCommands.Event}
+ */
+function action(event) {
+  const message = {
+    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+    message: "Performed action.",
+    icon: "Icon.80x80",
+    persistent: true,
+  };
+
+  // Show a notification message.
+  Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
+
+  // Be sure to indicate when the add-in command function is complete.
+  event.completed();
+}
+
+// Register the function with Office.
+Office.actions.associate("action", action);
